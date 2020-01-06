79f44a8d103d0b769b493823534f6248b46a382b
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-01-06 16:30:52 -0300

    Create sock_merchant.cs

---
+++ interview_preparation_kit/warm_up/sock_merchant.cs
@@ -0,0 +1,53 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+        int pair = 0;
+        int pos = 0;
+        int val = -1;
+        List<int> pairs = new List<int>();
+
+        for(int i = 0; i < n; i++){
+            val = ar[i];
+            pos = pairs.IndexOf(val);
+            if(pos != -1){
+                pair++;
+                pairs.RemoveAt(pos);
+            }
+            else {
+                pairs.Add(val);
+            }
+        }
+
+        return pair;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+        ;
+        int result = sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
