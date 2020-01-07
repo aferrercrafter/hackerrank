fb2c764b20275fc38a301b7b83a7a6b94852e821
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-01-07 17:19:05 -0300

    Create counting_valleys.cs

---
+++ interview_preparation_kit/warm_up/counting_valleys/counting_valleys.cs
@@ -0,0 +1,46 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, string s) {
+        int h = 0;
+        int v = 0;
+        int v_count = 0;
+
+        for(int i = 0; i < n; i++){
+            v = (s[i] == 'D')? -1 : 1;
+            v_count += (h == -1 && v == 1)? 1 : 0;
+            h += v;
+        }
+
+        return v_count;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string s = Console.ReadLine();
+
+        int result = countingValleys(n, s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
