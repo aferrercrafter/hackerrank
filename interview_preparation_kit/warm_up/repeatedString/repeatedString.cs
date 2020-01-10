ef2773af90f0ed46c8a57fc4f867d375d58f8505
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-01-10 18:21:25 -0300

    Create repeatedString.cs

---
+++ interview_preparation_kit/warm_up/repeatedString/repeatedString.cs
@@ -0,0 +1,36 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the repeatedString function below.
+    static long repeatedString(string s, long n) {
+        return (s.Count(x => x == 'a') * n / (long)s.Length) + (s.Substring(0, (int)(n % s.Length)).Count(x => x == 'a'));
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        long n = Convert.ToInt64(Console.ReadLine());
+
+        long result = repeatedString(s, n);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
