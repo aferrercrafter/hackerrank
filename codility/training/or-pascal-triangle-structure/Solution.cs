6a7e064f20395817175b28bc51a51321a11d142f
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-01-27 17:27:49 -0300

    Update Solution.cs

--- codility/training/or-pascal-triangle-structure/Solution.cs
+++ codility/training/or-pascal-triangle-structure/Solution.cs
@@ -1,37 +1,35 @@
 using System;
-// you can also use other imports, for example:
-// using System.Collections.Generic;
-
-// you can write to stdout for debugging purposes, e.g.
-// Console.WriteLine("this is a debug message");
 
 class Solution {
     public int solution(bool[] P) {
-        // write your code in C# 6.0 with .NET 4.5 (Mono)
-        int pascal_size = factorial(P.Length);
-        int false_seq = 0;
-        int not_true = 0;
         
-        for(int i = 0; i < P.Length - 1; i++){
-            
-            if(!P[i] && !P[i + 1])
-                false_seq ++;
-            else if(!P[i] && false_seq > 0)
-                not_true += factorial (false_seq + 1);
-            else if (!P[i] && false_seq == 0)
-                not_true ++;
-            else
-                false_seq = 0;
+        int size = Sum(P.Length);
+        int falseSeq = 0;
+        int falseCount = 0;
+        bool last = true;
+        
+        for(int i = 0; i < P.Length; i++){
+            if(P[i]){
+                if(!last){
+                    falseCount += Sum(falseSeq);    
+                }
+                falseSeq = 0;
+            }
+            else{
+                falseSeq++;
+                if(i == P.Length - 1){
+                    falseCount += Sum(falseSeq);
+                }
+            }
+            last = P[i];
         }
         
-        not_true += (!P[P.Length - 1] && P[P.Length - 2])? 1 : 0;
-        
-        return pascal_size - not_true;
+        return size - falseCount;
     }
     
-    int factorial (int i) {
-        if(i == 1)
+    int Sum (int i) {
+        if (i == 1)
             return 1;
-        return i + factorial(i - 1);
+        return i + Sum(i - 1);
     }
 }
