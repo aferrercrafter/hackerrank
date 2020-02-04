7de0e1933696ad890b93085a329a307e042bc700
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-02-04 11:57:32 -0300

    Create solution.cs

---
+++ codility/training/binary-gap/solution.cs
@@ -0,0 +1,29 @@
+using System;
+
+class Solution {
+    public int solution(int N) {
+        string b = Convert.ToString(N, 2);
+        
+        if(!b.Substring(1, b.Length - 1).Contains("1"))
+            return 0;
+        
+        int max = 0;
+        int seq = 0;
+        for(int i = 1; i < b.Length - 1; i++) {
+            if(b[i] == '0'){
+                seq++;
+                if(b[i + 1] == '1'){
+                    if(max < seq){
+                        max = seq;
+                        if(max >= b.Length - i - 1)
+                            break;
+                    }
+                        
+                    seq = 0;    
+                }
+            }
+        }
+            
+        return max;
+    }
+}
