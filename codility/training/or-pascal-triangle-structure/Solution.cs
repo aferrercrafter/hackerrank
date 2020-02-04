500a1eaed3f4bebf007f8450c4eb305fd8fa76dc
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-02-04 11:21:38 -0300

    Update Solution.cs

--- codility/training/or-pascal-triangle-structure/Solution.cs
+++ codility/training/or-pascal-triangle-structure/Solution.cs
@@ -3,33 +3,32 @@ using System;
 class Solution {
     public int solution(bool[] P) {
         
-        int size = Sum(P.Length);
-        int falseSeq = 0;
-        int falseCount = 0;
+        ulong size = Sum(Convert.ToUInt64(P.Length));
+        ulong falseSeq = 0;
+        ulong falseCount = 0;
         bool last = true;
         
         for(int i = 0; i < P.Length; i++){
             if(P[i]){
                 if(!last){
-                    falseCount += Sum(falseSeq);    
+                    falseCount += Sum(Convert.ToUInt64(falseSeq));    
                 }
                 falseSeq = 0;
             }
             else{
                 falseSeq++;
                 if(i == P.Length - 1){
-                    falseCount += Sum(falseSeq);
+                    falseCount += Sum(Convert.ToUInt64(falseSeq));
                 }
             }
             last = P[i];
         }
         
-        return size - falseCount;
+        ulong res = size - falseCount;
+        return (res >= 1000000000)? 1000000000 : (int)res;
     }
     
-    int Sum (int i) {
-        if (i == 1)
-            return 1;
-        return i + Sum(i - 1);
+    ulong Sum (ulong n) {
+        return (n * (n +1))/2;
     }
 }
