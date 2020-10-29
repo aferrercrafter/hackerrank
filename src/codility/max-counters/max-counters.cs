9823cf3625581852b7f280cfbc604ff5e8316c2f
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-10-28 23:02:33 -0300

    improved max counters, 100%

--- src/codility/max-counters/max-counters.cs
+++ src/codility/max-counters/max-counters.cs
@@ -9,28 +9,27 @@ namespace codility.max_counters
     {
         public int[] Solution(int N, int[] A)
         {
-            var counters = new Dictionary<int, int>();
-
-            for (int i = 0; i < N; i++)
-            {
-                counters.Add(i, 0);
-            }
-
+            var counters = new int[N];
             int max = 0;
+            int min = 0;
 
             foreach(int x in A)
             {
                 if (x != (N + 1))
                 {
+                    if (counters[x - 1] < min)
+                        counters[x - 1] = min;
                     counters[x - 1] = counters[x - 1] + 1;
                     max = Math.Max(counters[x - 1], max);
                 }
                 else
-                    for (int i = 0; i < N; i++)
-                        counters[i] = max;
+                    min = max;
             }
 
-            return counters.Select(x => x.Value).ToArray();
+            for (int i = 0; i < N; i++)
+                counters[i] = Math.Max(counters[i], min);
+
+            return counters;
         }
     }
 }
