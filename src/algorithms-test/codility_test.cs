4036d47a286e1f6b22a57f9153d28867c63803d7
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-11-01 16:04:23 -0300

    Added array rotation algorithm

--- src/algorithms-test/codility_test.cs
+++ src/algorithms-test/codility_test.cs
@@ -1,4 +1,5 @@
 using codility;
+using codility.array_rotation;
 using NUnit.Framework;
 
 namespace algorithms_test
@@ -6,15 +7,17 @@ namespace algorithms_test
     public class Tests
     {
         private BinaryGap _binaryGap;
+        private ArrayRotation _arrayRotation;
 
         [SetUp]
         public void Setup()
         {
             _binaryGap = new BinaryGap();
+            _arrayRotation = new ArrayRotation();
         }
 
         [Test]
-        public void TestBynary_Gap()
+        public void TestBynaryGap()
         {
             int s = _binaryGap.Solution(1041);
             Assert.AreEqual(5, s);
@@ -24,6 +27,19 @@ namespace algorithms_test
 
             s = _binaryGap.Solution(32);
             Assert.AreEqual(0, s);
+        }
+
+        [Test]
+        public void TestArrayRotration()
+        {
+            int[] s = _arrayRotation.Solution(new int []{ 3, 8, 9, 7, 6 }, 3);
+            Assert.That(new int[] { 9, 7, 6, 3, 8 }, Is.EqualTo(s));
+
+            s = _arrayRotation.Solution(new int[] { 0,0,0 }, 1);
+            Assert.That(new int[] { 0, 0, 0 }, Is.EqualTo(s));
+
+            s = _arrayRotation.Solution(new int[] { 1, 2, 3, 4 }, 4);
+            Assert.That(new int[] { 1, 2, 3, 4}, Is.EqualTo(s));
         }
     }
 }
---
+++ src/codility/array-rotation/array-rotation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codility.array_rotation
+{
+    public class ArrayRotation
+    {
+        public int[] Solution(int[] A, int K)
+        {
+            int[] S = new int[A.Length];
+
+            if (A == null)
+                return S;
+
+            if (A.Length == 0)
+                return S;
+
+            int p;
+            for (int i = 0; i < A.Length; i++)
+            {
+                p = (K + i) % A.Length;
+                S[p] = A[i];
+            }
+
+            return S;
+        }
+    }
+}
