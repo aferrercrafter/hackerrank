d9f25f5ce7e0b5c7ed3627fc7a4af41f0e8a7e78
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-11-01 13:57:21 -0300

    refactoring to csharp solution with tests, adding binary-gap problem

---
+++ src/algorithms-test/hackerrank_test.cs
@@ -0,0 +1,39 @@
+using hackerrank;
+using NUnit.Framework;
+
+namespace algorithms_test
+{
+    public class Tests
+    {
+        private BinaryGap _binaryGap;
+
+        [SetUp]
+        public void Setup()
+        {
+            _binaryGap = new BinaryGap();
+        }
+
+        [Test]
+        public void TestBynary_Gap()
+        {
+            int s = _binaryGap.Solution(1041);
+            Assert.AreEqual(5, s);
+
+            s = _binaryGap.Solution(15);
+            Assert.AreEqual(0, s);
+
+            s = _binaryGap.Solution(32);
+            Assert.AreEqual(0, s);
+        }
+
+        [Test]
+        public void TestZeroBynary_Gap()
+        { 
+            var s = _binaryGap.Solution(15);
+            Assert.AreEqual(0, s);
+
+            s = _binaryGap.Solution(32);
+            Assert.AreEqual(0, s);
+        }
+    }
+}
