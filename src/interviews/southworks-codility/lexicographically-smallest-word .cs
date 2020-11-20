62eda73569a47ccb92e5d7e98f1f022ff4a363c4
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-11-19 22:48:53 -0300

    added task 2 from southworks interview

---
+++ src/interviews/southworks-codility/lexicographically-smallest-word .cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace interviews.southworks_codility
+{
+    public class LexicographicallySmallestWord
+    {
+        /// <summary>
+        /// Lexicographically smallest string formed by removing at most one character
+        /// </summary>
+        /// <param name="S"></param>
+        /// <returns></returns>
+        public string Solution(string S)
+        {
+            int i;
+            for (i = 0; i < S.Length - 1; i++)
+            {
+                if (S[i] > S[i + 1])
+                    break;
+            }
+            return S.Remove(i, 1);
+        }
+    }
+}
